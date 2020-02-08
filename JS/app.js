25eadad1d2d7125a5b35ce3e284fb9092210aa92
dshapoval <dshapoval.epay@gmail.com>
2020-02-08 15:33:53 +0200

    added counter JS options

--- JS/app.js
+++ JS/app.js
@@ -3,3 +3,14 @@ console.log(productsCountEl);
 
 let addToCartButton = document.querySelectorAll(".add-to-cart");
 console.log(addToCartButton);
+
+addToCartButton.onclick = function() {
+	alert("Hello!")
+}
+
+for (let i=0; i < addToCartButton.length; i++) {
+	addToCartButton[i].addEventListener("click", function() {
+		let prevProductsCount = +productsCountEl.textContent;
+		productsCountEl.textContent = prevProductsCount + 1;
+	})
+}
