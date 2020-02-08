698fa99794b842e9f4db30628bac9e763b540f2c
dshapoval <dshapoval.epay@gmail.com>
2020-02-08 14:36:35 +0200

    added app.js

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+let productsCountEl = document.getElementById("products-count");
+console.log(productsCountEl);
+
+let addToCartButton = document.querySelectorAll(".add-to-cart");
+console.log(addToCartButton);
